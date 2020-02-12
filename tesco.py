8d73054935a772affa2d6aad53b7f9144014c49a
grag1337 <lqstismyname@gmail.com>
2020-02-12 17:14:08 +1000

    Update tesco.py

--- tesco.py
+++ tesco.py
@@ -23,7 +23,12 @@ tescoHousehold = "https://www.tesco.com/groceries/en-GB/shop/household/all" #/al
 
 def eatShit():
     url = "https://www.tesco.com/groceries/en-GB/shop/fresh-food/all"
-    hdr = {"User Agent": "Firefox/5.0"}
+    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
     req = Request(url, headers=hdr)
     page = urlopen(req).read()
     soup = BeautifulSoup(page, "html.parser")
