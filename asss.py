608b4a1e9306c992a8a004f8a6c5b2228a9becc6
grag1337 <47528119+grag1337@users.noreply.github.com>
2020-02-13 11:28:29 +1000

    Update asss.py

--- asss.py
+++ asss.py
@@ -25,7 +25,11 @@ auth.set_access_token("1227579454945255425-AF61yS6PyYxmnVXcld3TT6QgYY4AvI", "HhF
 api = tweepy.API(auth)
 
 url = "https://ghostbin.co/paste/j2d54/raw"
-hdr = {'User Agent': 'Mozilla/5.0'}
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+   'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+    'Accept-Encoding': 'none',
+    'Accept-Language': 'en-US,en;q=0.8',
+    'Connection': 'keep-alive'}
 req = Request(url, headers=hdr)
 page = urlopen(req)
 soup = BeautifulSoup(page, "html.parser")
